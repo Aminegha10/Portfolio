ac4d9ab9a12a7392e2fe83b5d216ac20f9f4c18f
amine <aminogha@gmail.com>
2025-03-22 23:48:34 +0000

    add vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 //theme provider
 import { ThemeProvider } from "@/components/ThemeProvider";
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
