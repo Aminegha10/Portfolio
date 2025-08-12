b129b7462a58f19019319c0c4796de830aefe88e
Mohamed Amine <aminogha@gmail.com>
2025-08-12 15:38:01 +0100

    Update page.jsx to improve component structure

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import Hero from "@/components/Hero";
 import Reviews from "@/components/Reviews";
 import Service from "@/components/Service";
 import Work from "@/components/Work";
-
+// 
 
 export default function Home() {
   return (
