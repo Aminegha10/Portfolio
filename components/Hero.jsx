8c1ab19a6c8a68e012bf58010db42fb49a241610
amine ghanim <aminogha@gmail.com>
2026-03-27 09:49:01 +0100

    Update Hero component to adjust text alignment for improved layout

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-5 md:flex-row md:justify-between gap-x-8">
           {/* text */}
-          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+          <div className="flex max-w-[600px] flex-col justify-start mx-auto xl:mx-0 text-center xl:text-left">
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
               Web Developer
             </div>
